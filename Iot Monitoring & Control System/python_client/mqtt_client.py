5b9a4c8441e918eec6ce5fc0d9e11c6dee26723e
Shwetha K C <shwethakc12@gmail.com>
2026-05-02 13:07:43 +0530

    Migrate MQTT client from Arduino to Python
    Converted the MQTT client from Arduino C++ to Python, implementing connection, message handling, and command input for controlling an LED.

--- Iot Monitoring & Control System/python_client/mqtt_client.py
+++ Iot Monitoring & Control System/python_client/mqtt_client.py
@@ -1,119 +1,39 @@
-#include <WiFi.h>
-#include <PubSubClient.h>
-#include <DHT.h>
-#include <ArduinoJson.h>
+import paho.mqtt.client as mqtt
+import threading
 
-// -------- WiFi --------
-const char* ssid = "Wokwi-GUEST";
-const char* password = "";
+BROKER = "broker.hivemq.com"
 
-// -------- MQTT --------
-const char* mqtt_server = "broker.hivemq.com";
-WiFiClient espClient;
-PubSubClient client(espClient);
+DATA_TOPIC = "shwetha/iot/data"
+CONTROL_TOPIC = "shwetha/iot/control"
 
-// -------- Topics --------
-const char* pub_topic = "shwetha/iot/data";
-const char* sub_topic = "shwetha/iot/control";
+# -------- MQTT Callbacks --------
+def on_connect(client, userdata, flags, rc):
+    print("\nConnected to MQTT Broker!")
+    client.subscribe(DATA_TOPIC)
+    print("Subscribed to data topic\n")
 
-// -------- LED --------
-#define LED_PIN 2
+def on_message(client, userdata, msg):
+    print("\nReceived:", msg.payload.decode())
+    print("Enter command (ON/OFF): ", end="", flush=True)
 
-// -------- Sensor --------
-#define DHTPIN 4
-#define DHTTYPE DHT22
-DHT dht(DHTPIN, DHTTYPE);
+# -------- MQTT Client --------
+client = mqtt.Client(protocol=mqtt.MQTTv311)
+client.on_connect = on_connect
+client.on_message = on_message
 
-// -------- WiFi --------
-void connectWiFi() {
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting WiFi");
+client.connect(BROKER, 1883, 60)
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+# Run MQTT in background thread
+client.loop_start()
 
-  Serial.println("\nWiFi Connected!");
-}
+print("Type ON or OFF to control LED\n")
 
-// -------- MQTT Callback --------
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message received: ");
+# -------- Input Loop --------
+while True:
+    cmd = input("Enter command (ON/OFF): ").strip().upper()
 
-  String message = "";
-  for (int i = 0; i < length; i++) {
-    message += (char)payload[i];
-  }
-
-  Serial.println(message);
-
-  // Control LED
-  if (message == "ON") {
-    digitalWrite(LED_PIN, HIGH);
-    Serial.println("LED ON");
-  } else if (message == "OFF") {
-    digitalWrite(LED_PIN, LOW);
-    Serial.println("LED OFF");
-  }
-}
-
-// -------- MQTT Connect --------
-void connectMQTT() {
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
-
-  while (!client.connected()) {
-    Serial.print("Connecting MQTT...");
-    if (client.connect("ESP32Client_Shwe")) {
-      Serial.println("Connected!");
-      client.subscribe(sub_topic);  // subscribe for control
-    } else {
-      Serial.print("Failed, rc=");
-      Serial.println(client.state());
-      delay(2000);
-    }
-  }
-}
-
-// -------- Send Data --------
-void sendMQTT(float temp, int light) {
-  StaticJsonDocument<200> doc;
-  doc["temperature"] = temp;
-  doc["light"] = light;
-
-  String payload;
-  serializeJson(doc, payload);
-
-  client.publish(pub_topic, payload.c_str());
-
-  Serial.println("Data Sent:");
-  Serial.println(payload);
-}
-
-// -------- Setup --------
-void setup() {
-  Serial.begin(115200);
-  pinMode(LED_PIN, OUTPUT);
-  dht.begin();
-
-  connectWiFi();
-  connectMQTT();
-}
-
-// -------- Loop --------
-void loop() {
-  if (!client.connected()) {
-    connectMQTT();
-  }
-  client.loop();
-
-  float temp = dht.readTemperature();
-  int light = analogRead(34);
-
-  if (!isnan(temp)) {
-    sendMQTT(temp, light);
-  }
-
-  delay(5000);
-}
+    if cmd in ["ON", "OFF"]:
+        client.publish(CONTROL_TOPIC, cmd)
+        print("Command sent:", cmd)
+    else:
+        print("Invalid command. Use ON/OFF")
